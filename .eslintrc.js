6542ed83949f524fe2d19535ae63fb5f0ae67ee3
Arif Datoo <arif@datoos.com>
2020-03-14 16:32:30 +0000

    add standard config for eslint, prettier and tsconfig

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'no-console': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
